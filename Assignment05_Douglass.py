757ff3e5faa0f6b9541c784dc356f63c61f35049
Sarah <douglass.s@northeastern.edu>
2024-03-01 16:30:08 -0500

    Setting up the first simple methods with their arguments as instructed by the assignment guide

--- Assignment05_Douglass.py
+++ Assignment05_Douglass.py
@@ -1,3 +1,18 @@
 #Assignment 05
 
-print("Hello")
+class BasicMathOperations:
+    
+    def GreetUser(first_name,last_name):
+        
+    def AddNumbers(number1,number2):
+        
+    def PerformOperations(num1,num2,num3,operator):
+        
+    def SquareNumber(number):
+        
+    def Factorial(number)
+    
+    def Counting(start_number,end_number):
+        
+    
+        
