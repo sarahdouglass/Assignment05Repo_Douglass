814d210dce6e3a499466cbc3027b9905a53dd8d4
Sarah <douglass.s@northeastern.edu>
2024-03-02 19:06:37 -0500

    Adding the method for the area of a rectangle calculation. Also moved def main to the top of the program. Also edited the implementation of each method to build off of the blank object user.

--- Assignment05_Douglass.py
+++ Assignment05_Douglass.py
@@ -1,5 +1,80 @@
 #Assignment 05
+def main():
+    user = BasicMathOperations()
+    answer = input("Hello! Welcome to Python, I have a set of questions and operations to share with you, do you want to hear them? Please enter Y for yes and N for no:")
+    
+    if answer == 'Y':
+        choice = int(input("""Great Choice! Here are your options, please enter the number of the prompt you'd like to interact with:
+        1.) Greet User  
+        2.) Add Numbers
+        3.) Perform Operation
+        4.) Square Numbers
+        5.) Factorial
+        6.) Counting
+        7.) Computing Hypotenuse
+        8.) Area of Rectangle
+        9.) Power of a Number
+        10.) Type of Argument"""))
+        
+        if choice == 1:
+            firstname = input("Please enter your first name:")
+            lastname = input("Please enter your last name:")
+            user.GreetUser(firstname,lastname)
+            
+        elif choice == 2:
+            number1 = float(input("Please enter a number:"))
+            number2 = float(input("Please enter another number:"))
+            user.AddNumbers(number1,number2)
+            
+        elif choice == 3:
+            number1 = float(input("Please enter a number."))
+            number2 = float(input("Please enter another number."))
+            operation = input("Next you need to enter an operation. Your options are addition(a), subtraction(s), multiplication(m), division(d), please enter one:")
+            print(f"\nThe answer using your desired operation is {user.PerformOperations(number1,number2,operation):.2f}")
+            
+        elif choice == 4:
+            number = float(input("Please enter the number you would like squared:"))
+            if number > 0:
+                print(f"The answer is {user.SquareNumber(number):.2f}")
+            else:
+                print("Invalid response, cannot square a negative number or zero!")
+            
+        elif choice == 5:
+            number = int(input("Please enter an integer, this program will find its factorial:"))
+            print(f"The factorial is {user.Factorial(number)}")
+            
+        elif choice == 6:
+            start = int(input("Please enter the starting number:"))
+            end = int(input("Please enter the ending number:"))
+            print(f"\nStarting from {start} and ending at {end}, your count is {user.Counting(start,end)}")
+            
+        elif choice == 7:
+            base = float(input("Please enter the value of the base of the right-angle triangle:"))
+            perpendicular = float(input("Please enter the value of the perpendicular of the right-angle triangle:"))
+            print(f" The value of the Hypotenuse is {user.calculateHypothesis(base,perpendicular):.2f}")
+            
+        elif choice == 8:
+            width = float(input("Please enter the width of the rectangle:"))
+            length = float(input("Please enter the length of the rectangle:"))
+            print(f"The area of the rectangle is {user.RectangleArea(width,length):.2f}")
+            
+        elif choice == 9:
+            base = float(input("Please enter the base number:"))
+            exponent = float(input("Please enter the exponent number:"))
+            print(f"The power based on a base of {base} and an exponent of {exponent} is {user.PowerNumber(base,exponent):.2f}")
+            
+        elif choice == 10:
+            argument = input("Please enter any variable, the type will be returned:")
+            print(f"The type is {user.ArgumentType(argument)}")
+            
+        else:
+            print("INVALID RESPONSE")
 
+    elif answer == 'N':
+        print("Rude.")  
+    else:
+        print("Invalid Answer!!!!")
+        
 class BasicMathOperations:
     
     def GreetUser(first_name,last_name):
@@ -12,15 +87,16 @@ class BasicMathOperations:
     def PerformOperations(num1,num2,operator):
         if num1 < num2:
             num1,num2 = num2,num1
-        if operation == 'd':
+        if operator == 'd':
             quotient = num1/num2
             return quotient
-        elif operation == 'm':
+        elif operator == 'm':
             product = num1*num2
             return product
-        elif operation == 'a':
+        elif operator == 'a':
             num_sum = num1 + num2
-        elif operation == 's':
+            return num_sum
+        elif operator == 's':
             difference = num1 - num2
             return difference
         
@@ -29,7 +105,7 @@ class BasicMathOperations:
         return answer
          
     def Factorial(number):
-        for i in range(1,n+1):
+        for i in range(1,number + 1):
             number = number*i
         return number
         
@@ -48,6 +124,8 @@ class BasicMathOperations:
         return hypo   
     
     def RectangeArea(width,length):
+        area = width*length
+        return area
         
     def PowerNumber(base,exponent):
         power = base**exponent
@@ -57,79 +135,4 @@ class BasicMathOperations:
         arg = type(argument)
         return arg
     
-def main():
-    user = BasicMathOperations()
-    answer = input("Hello! Welcome to Python, I have a set of questions and operations to share with you, do you want to hear them? Please enter Y for yes and N for no:")
-    
-    if answer == 'Y':
-        choice = int(input(f"""Great Choice! Here are your options, please enter the number of the prompt you'd like to interact with:
-        1.) Greet User  
-        2.) Add Numbers
-        3.) Perform Operation
-        4.) Square Numbers
-        5.) Factorial
-        6.) Counting
-        7.) Computing Hypotenuse
-        8.) Area of Rectangle
-        9.) Power of a Number
-        10.) Type of Argument"""))
-        
-        if choice == 1:
-            firstname = input("Please enter your first name:")
-            lastname = input("Please enter your last name:")
-            GreetUser(firstname,lastname)
-            
-        elif choice == 2:
-            number1 = float(input("Please enter a number:"))
-            number2 = float(input("Please enter another number:"))
-            AddNumbers(number1,number2)
-            
-        elif choice == 3:
-            number1 = float(input("Please enter a number."))
-            number2 = float(input("Please enter another number."))
-            operation = input("Next you need to enter an operation. Your options are addition(a), subtraction(s), multiplication(m), division(d), please enter one:")
-            print(f"\nThe answer using your desired operation is {PerformOperations(number1,number2,operation):.2f}")
-            
-        elif choice == 4:
-            number = float(input("Please enter the number you would like squared:"))
-            if number > 0:
-                print(f"The answer is {SquareNumber(number):.2f}")
-            else:
-                print("Invalid response, cannot square a negative number or zero!")
-            
-        elif choice == 5:
-            number = int(input("Please enter an integer, this program will find its factorial:"))
-            print(f"The factorial is {Factorial(number)}")
-            
-        elif choice == 6:
-            start = int(input("Please enter the starting number:"))
-            end = int(input("Please enter the ending number:"))
-            print(f"\nStarting from {start} and ending at {end}, your count is {Counting(start,end)}")
-            
-        elif choice == 7:
-            base = float(input("Please enter the value of the base of the right-angle triangle:"))
-            perpendicular = float(input("Please enter the value of the perpendicular of the right-angle triangle:"))
-            print(f" The value of the Hypotenuse is {calculateHypothesis(base,perpendicular):.2f}")
-            
-        elif choice == 8:
-            
-            
-        elif choice == 9:
-            base = float(input("Please enter the base number:"))
-            exponent = float(input("Please enter the exponent number:"))
-            print(f"The power based on a base of {base} and an exponent of {exponent} is {PowerNumber(base,exponent):.2f}")
-            
-        elif choice == 10:
-            argument = input("Please enter any variable, the type will be returned:")
-            print(f"The type is {ArgumentType(argument)}")
-            
-        else:
-            print("INVALID RESPONSE")
-
-    elif answer == 'N':
-        print("Rude.")  
-    else:
-        print("Invalid Answer!!!!")
-
-
 main()        
